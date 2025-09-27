38e7d9de8354d35447fbb53f11e94fa84fa7e9d5
pavan aitagoni <165299332+Pavan1214@users.noreply.github.com>
2025-09-27 07:17:56 +0530

    Update Image.js

--- models/Image.js
+++ models/Image.js
@@ -13,10 +13,12 @@ const ImageSchema = new mongoose.Schema({
   beforeImage: {
     url: { type: String, required: true },
     public_id: { type: String, required: true },
+    placeholder: { type: String, required: true }, // <-- ADD THIS LINE
   },
   afterImage: {
     url: { type: String, required: true },
     public_id: { type: String, required: true },
+    placeholder: { type: String, required: true }, // <-- ADD THIS LINE
   },
   createdAt: {
     type: Date,
